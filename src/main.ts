cfc3ea1f1e6347c31cbae361da57e16799895c0a
= <=>
2022-05-27 20:18:22 +0700

    use port as config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  console.log(process.env.PORT)
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
